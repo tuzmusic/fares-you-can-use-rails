d79aa2029c278bb81bcfcf4279c815c071f56c2c
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-16 11:52:32 +0200

    re-migrate DealAirports with airport_code property

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_01_16_095049) do
   create_table "deal_airports", force: :cascade do |t|
     t.integer "deal_id"
     t.integer "airport_id"
+    t.string "airport_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
