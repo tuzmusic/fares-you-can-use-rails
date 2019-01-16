37ac179bb63c422b05b43c6aa463d3c8125c6be5
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-16 14:04:48 +0200

    initial stub for DealsController#create

--- spec/controllers/deals_controller_spec.rb
+++ spec/controllers/deals_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe DealsController, type: :controller do
-
+  describe "create" do
+    it "creates a deal with given params" do
+      params = { 
+        deal: {
+          headline: "Great deals to NYC!",
+          description: "Direct round-trips to New York for under $100!",
+          start_date: ,
+          end_date: ,
+          instructions: "Google it! It's everywhere!",         
+        }
+      }
+    end
+  end
 end
