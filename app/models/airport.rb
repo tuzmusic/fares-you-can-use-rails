79bfc7c0bdcc867fca18370096dce59dfacc76c7
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-16 12:44:51 +0200

    build out import_from_gem with uniqueness

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -4,22 +4,28 @@ class Airport < ApplicationRecord
   
   def self.import_from_gem(gem_airport)
     a = gem_airport # shorter code
-    new_a = self.find_or_create_by(iata: a.iata)
-    new_a.name        = a.name
-    new_a.city        = a.city
-    new_a.country     = a.country
-    new_a.icao        = a.icao
-    new_a.latitude    = a.latitude
-    new_a.longitude   = a.longitude
-    new_a.altitude    = a.altitude
-    new_a.timezone    = a.timezone
-    new_a.dst         = a.dst
-    new_a.save
+
+    if self.find_by(iata: a.iata)
+      puts %("#{a.iata} already exists.")
+    else 
+      puts %(Importing "#{a.iata}"...)
+      new_a = self.create(
+        name:      a.name,
+        city:      a.city,
+        country:   a.country,
+        iata:      a.iata,
+        icao:      a.icao,
+        latitude:  a.latitude,
+        longitude: a.longitude,
+        altitude:  a.altitude,
+        timezone:  a.timezone,
+        dst:       a.dst)
+      puts "Failed." if not new_a.save
+    end
   end
 
   def self.import_all_from_gem
     Airports.all.each do |a|
-      puts "Importing \"#{a.iata}\"..."
       self.import_from_gem(a)
     end
   end
