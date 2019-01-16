5dd1fa78b69c106190d0cc40c2aa6a93b9f9d1d2
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-16 14:00:54 +0200

    stub Deal Views specs

--- spec/controllers/deals_controller_spec.rb
+++ spec/controllers/deals_controller_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe DealsController, type: :controller do
-  
-  describe "index" do
-    it "lists all the deals, with their description" do
-      
-    end
-    it "links to each deal's show page" do
-      
-    end
-  end
 
 end
---
+++ spec/views/deals_views_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "Deals Views", type: :feature do
+  
+  describe "index" do
+    it "lists all the deals, with their descriptions" do
+      
+    end
+    it "links to each deal's show page" do
+      
+    end
+    it "marks a deal as possibly expired if it's more than a week old" do
+      
+    end
+  end
+
+  describe "show" do
+    it "shows a deal's title, description, and instructions" do
+      
+    end
+    it "shows the start and end dates of a deal" do
+      
+    end
+    it "marks a deal as possibly expired if it's more than a week old" do
+      
+    end
+    it "shows the airports included in the deal" do
+      
+    end    
+    it "has some 'back' navigation" do
+      
+    end
+    it "does some other stuff too" do
+      expect(true).to eq(false)
+    end
+  end
+
+end
