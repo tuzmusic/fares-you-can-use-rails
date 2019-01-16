9ceb9ad18f2cb14c83d4efee3e39320956782b9d
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-16 14:43:39 +0200

    write spec for origin airports

---
+++ spec/models/origin_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Deals', type: :model do
+  it "has multiple origin airports" do
+    deal = Deal.create(headline:"Deal from DC")
+    deal.origins << Airport.iata("DCA")
+    deal.origins << Airport.iata("IAD")
+    deal.origins << Airport.iata("BWI")
+
+    expect(deal.origins.count).to eq(3)
+    expect(deal.origins.pluck :iata).to match(["DCA", "IAD", "BWI"])
+    expect(deal.origins.first).to be_an_instance_of(Airport)
+  end
+end
