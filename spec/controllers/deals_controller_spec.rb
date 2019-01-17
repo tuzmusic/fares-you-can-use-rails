a4562cf9e803a01d0bbad6dd705c4e3a71ddf48d
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-17 11:07:11 -0500

    work on mocking specs

--- spec/controllers/deals_controller_spec.rb
+++ spec/controllers/deals_controller_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe DealsController, type: :controller do
       params = { 
         deal: {
           headline: "Great deals to NYC!",
-          description: "Direct round-trips to New York for under $100!",
+          description: "Direct round-trips from DCA to New York for under $100!",
           start_date: ,
           end_date: ,
-          instructions: "Google it! It's everywhere!",         
+          instructions: "Google it! It's everywhere!",      
+          # how do I pass in Origins and Destinations? check other projects with join tables for their form's params.   
         }
       }
     end
