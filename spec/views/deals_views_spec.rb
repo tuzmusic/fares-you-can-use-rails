fdf4d67ed808d14dbf044257f9b6d4e4c4679525
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-18 09:40:55 -0500

    write deal show specs

--- spec/views/deals_views_spec.rb
+++ spec/views/deals_views_spec.rb
@@ -1,8 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "Deals Views", type: :feature do
+
+  describe "show" do
+      let(:deal) { Deal.create(
+        headline: "Great deals to NYC!",
+        description: "Direct round-trips from DC to New York for under $100! Good for the first week of February.",
+        start_date: Date.new(2019,2,1),
+        end_date: Date.new(2019,2,8),
+        instructions: "Google it! It's everywhere!",
+        origin_ids: [Airport.iata("DCA").id, Airport.iata("IAD").id, Airport.iata("BWI").id],  
+        destination_ids: [Airport.iata("EWR").id, Airport.iata("LGA").id, Airport.iata("JFK").id]) }
+
+    before :each do
+      visit deal_path(deal)
+    end
+
+    it "shows a deal's title, description, and instructions" do
+      expect(page).to have_content(deal.headline)
+      expect(page).to have_content(deal.description)
+      expect(page).to have_content(deal.instructions)
+    end
+    
+    it "shows the start and end dates of a deal" do
+      expect(page).to have_content "Feb. 1, 2019"
+      expect(page).to have_content "Feb. 8, 2019"
+    end
+
+    xit "marks a deal as possibly expired if it's more than a week old" do
+      
+    end
+
+    it "shows deal's origin airports" do
+      expect(page).to have_content "From: DCA, IAD, BWI"
+    end   
+    
+    it "shows deal's destination airports" do
+      expect(page).to have_content "To: EWR, LGA, JFK"
+    end  
+
+    xit "has some 'back' navigation" do
+      
+    end
+
+    it "does some other stuff, probably" do
+      expect(true).to eq(false)
+    end
+  end
   
   describe "index" do
+    before :each do
+      visit deals_path
+    end
     it "lists all the deals, with their descriptions" do
       
     end
@@ -14,28 +63,5 @@ RSpec.describe "Deals Views", type: :feature do
     end
   end
 
-  describe "show" do
-    it "shows a deal's title, description, and instructions" do
-      
-    end
-    it "shows the start and end dates of a deal" do
-      
-    end
-    it "marks a deal as possibly expired if it's more than a week old" do
-      
-    end
-    it "shows deal's origin airports" do
-      
-    end    
-    it "shows deal's destination airports" do
-      
-    end    
-    it "has some 'back' navigation" do
-      
-    end
-    it "does some other stuff too" do
-      expect(true).to eq(false)
-    end
-  end
 
 end
