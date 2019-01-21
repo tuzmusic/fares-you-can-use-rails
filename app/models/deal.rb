3f8e474dbc372f6bb0581815f537cdb3d56c598e
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-21 11:35:08 -0500

    write Deal#set_existing_blog_wrapper_for_date

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -23,6 +23,17 @@ class Deal < ApplicationRecord
   def destination_codes=(comma_separated_string)
     self.destinations = Deal.airports_from_string(comma_separated_string)
   end
+
+  def set_existing_blog_wrapper_for_date
+    deal_with_blog_wrapper = Deal.find do |deal|
+      deal.id != self.id && 
+      deal.posted_date == self.posted_date &&
+      deal.blog_head.present? || deal.blog_foot.present?
+    end
+    self.blog_head = deal_with_blog_wrapper.blog_head
+    self.blog_foot = deal_with_blog_wrapper.blog_foot
+    self.save
+  end
   
 end
 
--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Deal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "existing_blog_wrapper_for_date" do
+    # let(:deal_with_info) { 
+    #   Deal.create(posted_date: Date.today, blog_head: "Header", blog_foot: "Footer") 
+    # }
+    let(:new_deal) { Deal.create(posted_date: Date.today) }
+
+    it "finds a deal posted on the same date, with blog info and assigns the current deal's blog info to match the one it found" do
+      deal_with_info = Deal.create(posted_date: Date.today, blog_head: "Header", blog_foot: "Footer") 
+      new_deal.set_existing_blog_wrapper_for_date
+      expect(new_deal.blog_head).to eq(deal_with_info.blog_head)
+      expect(new_deal.blog_foot).to eq(deal_with_info.blog_foot)
+    end
+  end
 end
