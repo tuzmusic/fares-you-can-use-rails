0cead5abf55769ab06216aea8b6d03b3caad1d4d
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-21 11:03:02 -0500

    move instructions to helper method

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -10,4 +10,8 @@ module DealsHelper
   def deal_date_select(symbol)
     date_select :deal, symbol, order: [:month, :day, :year], start_year: Date.today.year, use_short_month: true 
   end
+
+  def instructions_for(deal)
+    "<p><b>How To Get It:</b><br>#{deal.instructions}</p>" unless deal.instructions.empty?
+  end
 end
