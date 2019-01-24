e3764eb1d3c802b499048d0dee11cc3143b0dadc
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-24 12:34:17 -0500

    spec real regions for airports and deals

--- spec/models/deal_region_spec.rb
+++ spec/models/deal_region_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Deal#region', type: :model do
+RSpec.describe "Setting a Deal's region with Deal#destinations=", type: :model do
   let(:cdg) { Airport.iata("CDG") }
   let(:mad) { Airport.iata("MAD") }
 
@@ -36,5 +36,43 @@ RSpec.describe 'Deal#region', type: :model do
       expect(d.region).to eq(nil)
     end
   end
+end
 
+RSpec.describe "Setting an airport's region" do
+
+  after :each do
+    Airport.where(name:nil).destroy
+  end
+
+  context "creating an airport" do
+    context "outside the US" do
+      it "sets the region based on the country" do
+        a = Airport.create(country: "France")
+        expect(a.region.name).to eq("Europe")
+      end
+    end
+    context "inside the US" do
+      it "sets the region based on the state" do
+        a = Airport.create(country: "United States", state: State.find_by(name:"Maine"))
+        expect(a.region.name).to eq("Northeastern USA")
+      end
+    end
+  end
+
+  context "setting a state for an existing airport" do
+    it "assigns that state's region to the airport's region" do
+      a = Airport.create(country: "United States")
+      a.update(state: State.find_by(name:"Maine"))
+      expect(a.region.name).to eq("Northeastern USA")
+    end
+  end
+
+  context "setting an airport's region manually" do
+    it "can be done" do
+      a = Airport.create(country: "France")
+      expect(a.region.name).to eq("Europe")
+      a.region = Region.find_by(name:"Africa")
+      expect(a.region.name).to eq("Africa")
+    end
+  end
 end
