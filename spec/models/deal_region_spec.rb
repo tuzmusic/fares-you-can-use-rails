4aa4c2305660007239c8f2d019ca9ea19519b1d2
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-24 12:00:32 -0500

    spec Deal#region

---
+++ spec/models/deal_region_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'Deal#region', type: :model do
+  let(:cdg) { Airport.iata("CDG") }
+
+  context "creating a deal with a destination" do
+    it "saves the deal with the region for that destination" do
+      d = Deal.create(destinations: [cdg])
+      expect(d.region).to eq(cdg.region)
+    end
+  end
+
+  context "assigning a destination via #destination_codes to an existing deal" do
+    it "sets the deal's region to the desination's region" do
+      d = Deal.create
+      d.destination_codes = "CDG"
+      expect(d.region).to eq(cdg.region)
+    end
+  end
+
+  context "assigning a destination via array assignment to an existing deal" do
+    it "sets the deal's region to the desination's region" do
+      d = Deal.create
+      d.destinations = [cdg]
+      expect(d.region).to eq(cdg.region)
+    end
+  end
+
+  context "deal without destinations" do
+    it "has no region assigned" do
+      d = Deal.create
+      expect(d.region).to eq(nil)
+    end
+  end
+
+end
