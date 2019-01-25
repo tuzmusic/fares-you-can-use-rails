c1376f2f3c2b2e604a633e0e6c87225e6abd8501
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-25 15:34:44 -0500

    add param: :slug to deal route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'regions/index'
   get 'regions/show'
-  resources :deals
+  resources :deals, param: :slug
   
   # resources :regions, param: :slug, only: [:index, :show] do
   #   resources :deals
