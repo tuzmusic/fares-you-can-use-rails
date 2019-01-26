29ab0f32d7aa35458dd5a2106736f5c026e7aa51
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-25 21:03:13 -0500

    deal#index with region, tests wrong

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,8 +1,29 @@
 module DealsHelper
+
+  # ---- INDEX/SHOW ---- 
   def brief_time(date)
     date&.strftime("%b. %e, %Y")
   end
 
+  def date_range(deal)
+    "#{brief_time(deal.start_date)} to #{brief_time(deal.end_date)}"
+  end
+
+  def instructions_for(deal)
+    "<p><b>How To Get It:</b><br>#{deal.instructions}</p>" unless deal&.instructions&.empty? || !deal.instructions
+  end
+
+  def deal_paragraphs_for_group(deals)
+    deals.map do |deal|
+      text = "<p>"
+      text += "<span class='warning'>POSSIBLY EXPIRED! - </span>" if deal.probably_expired?
+      text += link_to deal.headline, deal
+      text += "<br> Fly from " + date_range(deal) 
+      text += "</p>"
+    end.join
+  end
+
+  # ---- NEW/EDIT ---- 
   def airport_codes(airports)
     airports.map do |a|
       "#{a.iata} (#{a.name.chomp(" Airport")})" 
@@ -13,30 +34,11 @@ module DealsHelper
     date_select :deal, symbol, order: [:month, :day, :year], start_year: Date.today.year, use_short_month: true 
   end
 
-  def instructions_for(deal)
-    "<p><b>How To Get It:</b><br>#{deal.instructions}</p>" unless deal&.instructions&.empty? || !deal.instructions
-  end
-
-  def print_grouped_deals(grouped_deals_by_date)
-    grouped_deals_by_date.map do |(date, deals)|
-      %(<div id='deals-#{date.to_s}'>
-        <h3>#{brief_time date}</h3>
-          #{deal_paragraphs_for_group(deals)}</div>
-          )
-    end.join + "<div id='thing'/>"
-  end
-
-  def deal_paragraphs_for_group(deals)
-    deals.map do |deal|
-      %(<p>#{"<span class='warning'>POSSIBLY EXPIRED! -</span>" if deal.probably_expired?} #{link_to deal.headline, deal}</p>)
-    end.join
-  end
-
   def delete_deal_button
     button_to "Delete Deal", { action: "destroy", id: @deal.id }, method: :delete, data: { confirm: "Are you sure you want to delete this deal?" }
   end
 
   def edit_deal_button
     button_to "Edit Deal", { action: "edit", id: @deal.id }, method: :get
-  end
+  end  
 end
