29d1b220b028dc8ef03a9f32246766e0951afcda
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-28 17:31:03 -0500

    stub "fb login for existing user" specs

--- spec/features/omniauth_spec.rb
+++ spec/features/omniauth_spec.rb
@@ -2,15 +2,48 @@ require 'rails_helper'
 require_relative '../spec_helpers/omniauth_helper'
 
 describe "Facebook login" do
-  before :each do
-    valid_facebook_login_setup
-    # get "auth/facebook/callback"
-    # request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+  context "brand new user" do
+    before :each do
+      valid_facebook_login_setup
+      visit root_path
+      click_on "Sign in with Facebook"
+    end
+
+    it "successfully logs in using Facebook" do
+      expect(page).to have_content "John Doe"
+    end
+
+    it "creates a new user" do
+      expect(User.count).to eq 1
+    end
   end
 
-  it "successfully logs in using Facebook" do
-    visit root_path
-    click_on "Sign in with Facebook"
-    binding.pry
+  context "existing email user" do
+    before :each do
+      User.create(email: "test@example.com", password:"123456")
+      expect(User.last.email).to eq "test@example.com"
+      valid_facebook_login_setup
+      visit root_path
+      click_on "Sign in with Facebook"
+    end
+
+    it "does not create a new user" do
+      expect(User.count).to eq 1
+    end
+
+    it "successfully logs in that user" do
+      expect(page).to have_content "test@example.com"
+    end
+
+    it "assigns their name from their FB info" do
+      expect(page).to have_content "John Doe"
+    end
+
+
   end
+  
+  context "new email user for existing facebook user" do
+    
+  end
+
 end
