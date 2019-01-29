0209948c108833945aa5700fa646d6d0910c8363
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-29 10:21:44 -0500

    refactor User.from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,15 @@ class User < ApplicationRecord
 
 
   def self.from_omniauth(auth)
-    if user = User.find_by(email: auth.info.email)
+    find_or_create_by(email: auth.info.email).tap do |user|
       user.provider    ||= auth.provider
       user.uid         ||= auth.uid
+      user.email       ||= auth.info.email
+      user.password    ||= Devise.friendly_token[0,20]
       user.first_name  ||= auth.info.first_name   
       user.last_name   ||= auth.info.last_name   
       commented_stuff_for_omniauth(user)
       user.save
-      user
-    elsif user = User.create(provider: auth.provider, uid: auth.uid)
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.first_name   
-      user.last_name = auth.info.last_name   
-      commented_stuff_for_omniauth(user)
-      user.save 
-      user
     end
   end
          
