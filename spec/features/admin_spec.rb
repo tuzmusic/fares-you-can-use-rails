ef0d2e41ff9459bc04c531149943977d0bd6a8fa
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-30 09:56:06 -0500

    remove user auth from deals show/index

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -8,37 +8,55 @@ describe "Admin" do
     end
   end
 
-  describe "logging in" do
-    it "allows an admin to log in from /admin" do
+  describe "advanced Devise implementation" do
+    xit "lives at /admin" do
       visit '/admin'
+      expect(page).to have_field "Email"
+    end
+    
+    xit "has a custom login page" do
+      visit new_admin_session_path
+      expect(page).to_not have_content "Sign up"
+    end
+
+    it "doesn't replace the users path!!!" do
+      visit new_user_session_path
+      expect(current_path).to eq new_user_session_path  
+    end
+  end
+
+  describe "logging in" do
+    
+    it "allows an admin to log in from /admins" do
+      visit new_admin_session_path
       expect(page).to have_field "Email"
     end    
 
     it "does not allow an admin to be created from the website" do
-      visit '/admin/new'
-      expect(page).to have_content 'No route matches [GET] "/admin/new"'
+      expect{visit '/admins/new'}.to raise_error(ActionController::RoutingError)
     end
 
     it "redirects to the admin portal" do
       create_and_log_in_admin
-      expect(page).to have_content "Admin Portal"
+      expect(current_path).to eq "/deals" 
+      # expect(page).to have_content "Admin Portal"
     end
 
     it "allows an existing admin to use facebook to log in" do
       create_admin
-      visit '/admin'
+      visit new_admin_session_path
       click_link "Facebook"
       expect(page).to have_content "Admin Portal"
     end
 
     it "does not allow facebook login for someone who isn't an admin" do
-      visit '/admin'
+      visit new_admin_session_path
       click_link "Facebook" # don't need to create user because facebook login succeeds automatically in test mode
       expect(current_path).to eq new_user_session_path 
     end
   end
 
-  describe "Admin Portal (/admin/deals, probably)" do
+  describe "Admin Portal (/admins/deals, probably)" do
     before :each do
       create_and_log_in_admin
     end
@@ -73,11 +91,14 @@ describe "Admin" do
   end
   
   def create_admin
-    
+    Admin.create(email: "test@example.com", password:"123456")
   end
 
   def log_in_admin
-    
+    visit new_admin_session_path
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "123456"
+    click_on "Log in"
   end
 
   def create_and_log_in_admin
