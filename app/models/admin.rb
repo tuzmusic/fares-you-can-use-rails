ededd40df2c5c9cd4d41ba1d0e5297d1e1aa31c5
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-30 13:45:44 -0500

    add first, last, full name to admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,9 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  def full_name
+    [first_name, last_name].join(" ") if first_name && last_name
+  end  
+  
 end
