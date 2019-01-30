05c94a11d0170cab77a58bfdf8cef5231bbc9d22
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-30 12:12:36 -0500

    create simpler (root) sign in/out/up routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }  
-  devise_scope :user do get "/users/sign_out" => 'devise/sessions#destroy' end
+  devise_scope :user do 
+    get "/sign_out" => 'devise/sessions#destroy', as: 'user_sign_out'
+    get "/sign_in" => 'devise/sessions#new', as: 'user_sign_in'
+    get "/sign_up" => 'devise/registrations#new', as: 'user_sign_up'
+  end
 
   devise_for :admins, path: 'admin'
 
