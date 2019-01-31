ea33b577a029aec785b2235a89bf32bf8e81a3c7
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-31 17:01:07 -0500

    fix edit button on index

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,2 +1,21 @@
 class PreferencesController < ApplicationController
+
+  before_action do
+    set_prefs
+  end
+  def index
+  end
+
+  def edit
+    @selected_vacation = @prefs.vacations.find(params[:vacation_id])
+  end
+
+  def update
+    raise params
+  end
+
+  def set_prefs
+    @prefs = Preference.find_by(user_id: current_user.id)
+  end
+
 end
