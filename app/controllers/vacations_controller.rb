ff87cd90fec40c62529a3ca863fd3c0596897855
Jonathan Tuzman <tuzmusic@gmail.com>
2019-01-31 17:22:30 -0500

    basic single vacation edit flow complete!!!

--- app/controllers/vacations_controller.rb
+++ app/controllers/vacations_controller.rb
@@ -1,6 +1,12 @@
 class VacationsController < ApplicationController
   def update
+    # raise params
     vacation = Vacation.find(params[:id])
+    vacation.update(vacation_params)
     redirect_to '/preferences'
+  end
+
+  def vacation_params
+    params.require(:vacation).permit :name, :start_date, :end_date
   end
 end
