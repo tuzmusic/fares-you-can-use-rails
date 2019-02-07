fd909b5388f0c30eeb586fc0d9609d7bbfff220d
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-07 10:11:03 -0500

    add dates for vacations on indexes

--- app/helpers/regions_helper.rb
+++ app/helpers/regions_helper.rb
@@ -1,6 +1,6 @@
 module RegionsHelper
   def region_with_deals_count(region)
     text = "#{region.name} (#{pluralize region.deals.count, 'deal'})"
-    link_to text, region_deals_path(region)
+    link_to text, region_deals_path(region, params:{filter:true})
   end
 end
