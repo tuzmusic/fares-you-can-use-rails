b72d61da465caae30356ea0396396a33440eb0ba
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-11 09:33:17 -0500

    add note for favorited deal on show page

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ApplicationRecord
   belongs_to :preference
   belongs_to :deal
+
+  def self.for_deal(deal, user)
+    where(deal_id: deal.id, preference_id: user.preferences.id).first
+  end
 end
