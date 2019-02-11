7bd55fe7adcb509540893c03af2af44c455a5fbe
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-11 09:04:19 -0500

    write favorites#new page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :deals, param: :slug do
     resources :favorites
   end
+  resources :favorites
+  
   get 'my-deals', to: 'deals#my_deals', as: :my_deals_path
   resources :regions, param: :slug, only: [:index, :show] do
     resources :deals, only: [:index, :show], param: :slug
