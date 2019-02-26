516fe1ab9e8b49396b26014466e4f656e7346c6c
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-26 18:26:10 -0500

    add regions to user serializer

--- app/serializers/preference_serializer.rb
+++ app/serializers/preference_serializer.rb
@@ -1,5 +1,6 @@
 class PreferenceSerializer < ActiveModel::Serializer
   attributes :id
   has_many :home_airports
+  has_many :regions
   belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :provider, :first_name, :last_name
   has_many :home_airports
+  has_many :regions
+  has_one :preferences
 end
