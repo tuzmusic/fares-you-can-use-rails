8c592c710413c562ce716e40ccc99dad143694e9
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-26 14:44:05 -0500

    rmeove some comments

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -12,9 +12,4 @@ class FavoritesController < ApplicationController
   def favorite_params
     params.require(:favorite).permit :preference_id, :deal_id, :comment
   end
-  
-  # deal show page include add to favorites LINK
-  # new favorite includes a field for comment (that's the only real important thing)
-  # nest favorites route (deal/deal_slug/favorites/new)
-  # deal id/slug in fave#new form as hidden field (Also preference, which we can get from current user)
 end
