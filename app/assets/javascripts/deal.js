49466c1f073a51ca108e2731060d1ccd784ed992
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-27 17:12:19 -0500

    beginnings of object conversion for deal, with tests

--- app/assets/javascripts/deal.js
+++ app/assets/javascripts/deal.js
@@ -1,17 +1,24 @@
 class Deal {
- /*
+  /*
    attributes :id, :headline, :description, :start_date, :end_date, :links, :region_name
   has_many :origins
   has_many :destinations
 
- */ 
+ */
 
- constructor() {
-
- }
-
- helloWorld() {
-   return "hello world"
- }
-
+  constructor({
+    id,
+    headline,
+    description,
+    start_date,
+    end_date,
+    region_name
+  }) {
+    this.id = id;
+    this.headline = headline;
+    this.description = description;
+    this.start_date = start_date;
+    this.end_date = end_date;
+    this.region_name = region_name;
+  }
 }
--- spec/javascripts/classes_spec.js
+++ spec/javascripts/classes_spec.js
@@ -1,10 +1,45 @@
-describe('Deals class', () => {
-  it('has a helloWorld() function', () => {
-    let deal = new Deal()
-    expect(deal.helloWorld()).to.equal("hello world")
-  });
-  it('constucts a deal', () => {
-    let deal = new Deal()
-    expect(deal).to.be.an.instanceof(Deal)
+describe("Deals class", () => {
+  // it('constucts a deal', () => {
+  //   let deal = new Deal()
+  //   expect(deal).to.be.an.instanceof(Deal)
+  // });
+
+  it("contructs a Deal object from a json", () => {
+    let json = {
+      id: 6,
+      headline: "IAD to Beijing for $535 non-stop round trip - United Airlines",
+      description:
+        'Departure dates available from February 20 to April 6 almost daily. Check out the calendar \u003ca href="http://mail01.tinyletterapp.com/FaresYouCanUse/today-s-flights-shanghai-beijing-moscow-lisbon/13513657-www.google.com/flights?c=cf040a38-8c96-4f9b-9338-8c223c053a53"\u003ehere\u003c/a\u003e.',
+      start_date: "2019-02-20",
+      end_date: "2019-04-06",
+      links: [],
+      region_name: "Asia",
+      origins: [
+        {
+          id: 2656,
+          name: "Washington Dulles International Airport",
+          city: "Washington",
+          country: "United States",
+          iata: "IAD"
+        }
+      ],
+      destinations: [
+        {
+          id: 2324,
+          name: "Beijing Capital International Airport",
+          city: "Beijing",
+          country: "China",
+          iata: "PEK"
+        }
+      ]
+    };
+    let deal = new Deal(json);
+    // debugger
+    expect(deal.id).to.equal(deal['id'])
+    expect(deal.headline).to.equal(deal['headline'])
+    expect(deal.description).to.equal(deal['description'])
+    expect(deal.start_date).to.equal(deal['start_date'])
+    expect(deal.end_date).to.equal(deal['end_date'])
+    expect(deal.region_name).to.equal(deal['region_name'])
   });
 });
