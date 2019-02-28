21afa68a7f1033eefb4590074b7b04cd7909a48e
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-28 13:51:34 -0600

    add some tests

--- spec/javascripts/deals_index_spec.js
+++ spec/javascripts/deals_index_spec.js
@@ -4,9 +4,15 @@ describe('Deals index', () => {
     MagicLamp.load('deals/index')  
   })
 
-  describe('myDeals', () => {
-    it('returns true', () => {
-      expect($('#myDealsHeader')[0].innerHTML).to.include("Deals")
+  describe('deals index', () => {
+    it('shows the "My Deals" header on load when logged in', () => {
+      expect($("#my-deals-active-all-deals-linked")[0].hidden).to.equal(false)
+      expect($("#all-deals-active-my-deals-linked")[0].hidden).to.equal(true)
+    });
+    it('switches headers when you click on "All Deals', () => {
+      $("#my-deals-active-all-deals-linked a")[0].click()
+      expect($("#my-deals-active-all-deals-linked")[0].hidden).to.equal(true)
+      expect($("#all-deals-active-my-deals-linked")[0].hidden).to.equal(false)
     });
   });
 });
