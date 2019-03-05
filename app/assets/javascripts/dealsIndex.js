7488873349af16f28b28b7e89679bd39d03a909d
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-05 12:29:42 -0500

    display only All Deals when signed out

--- app/assets/javascripts/dealsIndex.js
+++ app/assets/javascripts/dealsIndex.js
@@ -1,4 +1,5 @@
 function myDeals() {
+  $("#all-deals-signed-out")[0].hidden = true;
   $("#all-deals-active-my-deals-linked")[0].hidden = true;
   $("#my-deals-active-all-deals-linked")[0].hidden = false;
 
@@ -13,32 +14,45 @@ function myDeals() {
 }
 
 let allDealsList;
-function allDeals() {
-  $("#my-deals-active-all-deals-linked")[0].hidden = true;
-  $("#all-deals-active-my-deals-linked")[0].hidden = false;
-  
-  if (!allDealsList) $.get("api/deals", deals => (allDealsList = deals));
+function allDeals(signedOut) {
+  // debugger
+  if (allDealsList) {
+    $("#all-deals-signed-out")[0].hidden = !signedOut;
+    $("#all-deals-active-my-deals-linked")[0].hidden = signedOut;
+    $("#my-deals-active-all-deals-linked")[0].hidden = true;
 
-  $("#deals").html(
-    "<br>" +
-      allDealsList
-        .map(d => new Deal(d))
-        .sort((a, b) => {
-          b.posted_date > a.posted_date;
-        })
-        .map(d => d.indexParagraph())
-        .join("")
-  );
+    $("#deals").html(
+      "<br>" +
+        allDealsList
+          .map(d => new Deal(d))
+          .sort((a, b) => {
+            b.posted_date > a.posted_date;
+          })
+          .map(d => d.indexParagraph())
+          .join("")
+    );
+  } else {
+    $.get("api/deals", deals => {
+      allDealsList = deals;
+      allDeals(signedOut);
+    });
+  }
 }
 
 let user;
 $(function() {
-  if (true) {
-    myDeals();
-    fetch("api/deals")
-      .then(res => res.json())
-      .then(json => {
-        allDealsList = json;
-      });
-  }
+  $.get("api/user", user => {
+    if (user) {
+      debugger
+      myDeals();
+      fetch("api/deals")
+        .then(res => res.json())
+        .then(json => {
+          allDealsList = json;
+        });
+    } else {
+      debugger
+      allDeals(true);
+    }
+  });
 });
