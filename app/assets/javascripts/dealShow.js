04e9945d2553fa093a232440caf1b351e86f7bb4
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-05 20:42:01 -0500

    fix deals nav link

--- app/assets/javascripts/dealShow.js
+++
@@ -1,7 +0,0 @@
-/* $(function () {
-  let slug = location.pathname.split('/').slice(-1)[0]
-  $.get(`/api/deals/${slug}`, res => {
-    let deal = new Deal(res)
-    $('#deal').html(deal.showPageParagraph())
-  })
-}) */
