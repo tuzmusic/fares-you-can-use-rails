2ea821adc99a8500112e5f108047a542bd9e288e
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-05 20:29:44 -0500

    move dealShow to show view

--- app/assets/javascripts/dealShow.js
+++ app/assets/javascripts/dealShow.js
@@ -1,11 +1,7 @@
-$(function () {
-  // HOW DO I ONLY LOAD THIS SCRIPT ON THE SHOW PAGE?
-  // Fine for now because I /have/ circumvented the dealsIndex script which is the only other one I have at the moment.
-  if (location.pathname == "/" || location.pathname == "deals/") return
-
+/* $(function () {
   let slug = location.pathname.split('/').slice(-1)[0]
   $.get(`/api/deals/${slug}`, res => {
     let deal = new Deal(res)
     $('#deal').html(deal.showPageParagraph())
   })
-})
+}) */
