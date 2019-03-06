31dc9d30392bf40f85f91713403bfa7bc9245236
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-06 14:59:27 -0500

    remove old vacation list implementation and partial

--- app/assets/javascripts/prefsVacations.js
+++ app/assets/javascripts/prefsVacations.js
@@ -2,7 +2,7 @@ function showVacations(vacations) {
   let list = vacations
     .map(v => new Vacation(v))
     .map(v => v.vacationsListParagraph());
-  $("p#vacations").html(
+  $("#vacations-list").html(
     vacations.length > 0 ? list : "You haven't added any vacations."
   );
 }
