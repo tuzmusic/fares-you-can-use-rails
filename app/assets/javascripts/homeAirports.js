d4d9d887dd83a915f0bbc19d771d8daaeccab035
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-06 13:17:05 -0500

    add home airports dynamically on preferences page

--- app/assets/javascripts/homeAirports.js
+++ app/assets/javascripts/homeAirports.js
@@ -3,8 +3,9 @@ function addHomeAirport() {
   let iata = input.value.slice(0, 3)
   // Get user, because for some reason current_user is nil in the post request?!
   $.get('/api/user', res => {
-    $.post('/api/user/airports', {iata: iata, user_id: res.id}).done((res) => {
-      debugger
+    $.post('/api/user/airports', {iata: iata, user_id: res.id}).done((airports) => {
+      let list = airports.map(a =>`<li>${a.name} (${a.iata})</li>`)
+      $('#home-airports-list').html(list)
     })
   })
 }
